62f4a7b0ad8bb6e842f1d584c7f8545a2c967965
Costi <mail.la.costi@gmail.com>
2018-10-04 10:57:08 +0300

    the store in action

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>TDD with Redux</div>, document.getElementById('root'));
+// ReactDOM.render(<div>TDD with Redux</div>, document.getElementById('root'));
+
+import store from './store'
+import { VANILLA } from "./constants/flavors";
+import { addProductToFreezer, updateTemperature } from './actions/freezer';
+
+store.subscribe(() => console.log(store.getState()))
+
+store.dispatch(updateTemperature(-8))
+store.dispatch(addProductToFreezer(VANILLA, 17))
