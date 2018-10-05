dbb01d91b2c233ebf808cee55a9c984c7343ecb6
Costi <mail.la.costi@gmail.com>
2018-10-05 16:27:50 +0300

    testing thunks

--- src/ducks/__tests__/freezer/actions.js
+++ src/ducks/__tests__/freezer/actions.js
@@ -33,3 +33,27 @@ describe('addProductToFreezer()', () => {
     expect(action.payload.amount).toEqual(20)
   })
 })
+
+describe('doSomething()', () => {
+  const getState = () => ({
+    freezer: {
+      temperature: 10
+    }
+  })
+
+  it('should dispatch the "BUZZ" action', () => {
+    const dispatch = jest.fn()
+    const action = actions.doSomething()
+    action(dispatch, getState)
+    expect(dispatch.mock.calls.length).toEqual(1)
+    expect(dispatch.mock.calls[0][0].type).toEqual('BUZZ')
+  })
+
+  it('should use the temperature in the state for the payload', () => {
+    const dispatch = jest.fn()
+    const action = actions.doSomething()
+    action(dispatch, getState)
+    expect(dispatch.mock.calls.length).toEqual(1)
+    expect(dispatch.mock.calls[0][0].payload).toEqual(10)
+  })
+})
