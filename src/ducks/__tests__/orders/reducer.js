76d3a4ebbea3e927cc10c771be813952d0a92130
Costi <mail.la.costi@gmail.com>
2018-10-05 13:50:15 +0300

    adding orders

---
+++ src/ducks/__tests__/orders/reducer.js
@@ -0,0 +1,22 @@
+import { reducer, actions } from '../../orders';
+import { VANILLA } from '../../../constants/flavors';
+
+describe('Orders reducer', () => {
+  it('should store the order in the state', () => {
+    const newState = reducer(undefined, actions.placeOrder({
+      customerName: 'Cindy',
+      cone: false,
+      scoops: {
+        [VANILLA]: 1
+      }
+    }))
+    expect(newState.length).toEqual(1)
+    expect(newState[0].customerName).toEqual('Cindy')
+    expect(newState[0].status).toEqual('pending')
+    expect(newState[0].cone).toEqual(false)
+    expect(newState[0].scoops).toEqual({
+      [VANILLA]: 1
+    })
+    expect(typeof newState[0].createdAt).toEqual('number')
+  })
+})
